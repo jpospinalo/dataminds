885515b7e808cc105a0890cbfae452d2ca3e0abf
jpospinalo <jpospinalo@unal.edu.co>
2024-09-16 20:18:24 -0500

    Add files via upload

---
+++ app/database.py
@@ -0,0 +1,10 @@
+from sqlmodel import create_engine
+
+username = 'admin'
+password = 'adminadmin'
+host = 'proyecto-dataminds.cfzybbinygh8.us-east-1.rds.amazonaws.com'
+database = 'DataMinds'
+ 
+
+# Create a SQLModel engine
+engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{host}:3306/{database}')
