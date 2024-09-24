358452dbe9b4568d26e9f0d69731ca8830277366
jpospinalo <jpospinalo@unal.edu.co>
2024-09-23 19:40:43 -0500

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import Field, SQLModel, create_engine
-
-
-class Client(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    first_name: str
-    last_name: str
-    age: int 
-
