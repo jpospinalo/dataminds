0fad9a6fcdcee4083d4d72940072f3f99b03d896
dalohuer2004 <dalohuer2004@disroot.org>
2024-11-14 02:55:15 +0000

    completo

--- app/models.py
+++ app/models.py
@@ -7,10 +7,10 @@ class Client(SQLModel, table=True):
  
     id_cliente: int | None = Field(default=None, primary_key=True)
     tipo_documento: str 
-    primer_nombre_cliente: str
-    segundo_nombre_cliente: str | None = None
-    primer_apellido_cliente: str
-    segundo_apellido_cliente: str | None = None
+    primer_nombre: str
+    segundo_nombre: str | None = None
+    primer_apellido: str
+    segundo_apellido: str | None = None
 
 # Conexión con la base de datos de la tabla Empresa.
 class Empresa(SQLModel, table=True):
@@ -26,7 +26,7 @@ class Obligacion(SQLModel, table=True):
     __tablename__ = "obligacion"
  
     id_obligacion: int | None = Field(default=None, primary_key=True)
-    id_cliente: str
-    id_empresa: str 
+    id_cliente: int
+    id_empresa: int 
     
     
--- app/schemas.py
+++ app/schemas.py
@@ -3,10 +3,10 @@ from pydantic import BaseModel
 class BaseClient(BaseModel):
     id_cliente: int 
     tipo_documento: str #| Field(str= cedula, pasaporte, cedula extranjeria, ppt)
-    primer_nombre_cliente: str
-    segundo_nombre_cliente: str | None = None
-    primer_apellido_cliente: str
-    segundo_apellido_cliente: str | None = None
+    primer_nombre: str
+    segundo_nombre: str | None = None
+    primer_apellido: str
+    segundo_apellido: str | None = None
 
 class ClientIn(BaseClient):
     pass
@@ -26,8 +26,8 @@ class EmpresaOut(BaseEmpresa):
 
 class BaseObligacion(BaseModel):
     id_obligacion: int 
-    id_cliente: str
-    id_empresa: str 
+    id_cliente: int
+    id_empresa: int 
     
 class ObligacionIn(BaseObligacion):
     pass
